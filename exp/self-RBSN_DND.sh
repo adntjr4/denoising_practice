56e3cff96ee5f7436f41e3af78d3ddf30ce2c67a
woos <adntjr4@gmail.com>
2021-05-31 20:11:10 +0900

    .

---
+++ exp/self-RBSN_DND.sh
@@ -0,0 +1,20 @@
+resume="False"
+n_thread="4"
+while getopts g:t:r opt
+do
+   case "$opt" in
+      g) gpu_id="$OPTARG";;
+      t) n_thread="$OPTARG";;
+      r) resume="True";;
+   esac
+done
+
+##### Setting #####
+
+session="self-RBSN_DND"
+config="self-RBSN_DND"
+
+###################
+
+cd ..
+python train.py --session_name $session --config $config --resume $resume --gpu $gpu_id --thread $n_thread
